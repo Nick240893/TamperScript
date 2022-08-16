8372794c4d462fb48c596660c4af701bba747309
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-08-16 15:03:42 +0200

    Create CA Automic Reload.user.js

---
+++ CA Automic Reload.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         CA Automic reload
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       NigroN
+// @include      http://10.183.247.68*
+// @include      http://10.183.247.69*
+// @include      http://10.147.247.68*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    document.addEventListener("visibilitychange", () => {
+        if (document.visibilityState == "visible" && window.location.href.includes("overview&")) {
+            document.location.reload(true);
+        }
+    });
+})();
