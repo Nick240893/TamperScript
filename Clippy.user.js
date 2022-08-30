ee3e5ca903fa7e14537df9aadae16023fc1f4af8
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-08-30 10:59:42 +0200

    Update Clippy.user.js

--- Clippy.user.js
+++ Clippy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Clippy
 // @namespace    http://tampermonkey.net/
-// @version      2.5
+// @version      2.6
 // @description  try to take over the world!
 // @author       NigroN
 // @match        https://ts.accenture.com/*
