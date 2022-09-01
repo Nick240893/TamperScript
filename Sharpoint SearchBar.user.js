5daa2cef5ba06c68fa94262f7eda9c0f47b58387
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-09-01 03:51:02 +0200

    Update Sharpoint SearchBar.user.js

--- Sharpoint SearchBar.user.js
+++ Sharpoint SearchBar.user.js
@@ -1,52 +1,56 @@
 // ==UserScript==
 // @name         Sharpoint SearchBar
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.5
 // @description  try to take over the world!
 // @author       NigroN
 // @include      https://ts.accenture.com/*
 // @grant        none
+// @require      https://checkapp2.pages.dev/Dipendenze/jQueryUI-Cupertino/external/jquery/jquery.js
 // ==/UserScript==
 
 (function() {
 	async function search(string){
-		var tab = document.getElementById("layoutsTable").getElementsByTagName("a");
+		var els = document.getElementById("layoutsTable").getElementsByTagName("a");
 		if (!(input.value == "")) await hide("none");
-		for (var X=0; X<tab.length; X++) {
-			if ((tab[X].href.toUpperCase().replace(/.*\//gi,"").replace(/\.[A-Z]*$/gi,"").includes(string.toUpperCase())) || (tab[X].lastChild.alt.toUpperCase().replace(/\.[A-Z]*/g,"").includes(string.toUpperCase()))) {
-				tab[X].style.display="";
-				tab[X].parentNode.style.display="";
+		 for (var el of els) {
+			if ((el.href.toUpperCase().replace(/.*\//gi,"").replace(/\.[A-Z]*$/gi,"").includes(string.toUpperCase())) || (el.lastChild.alt.toUpperCase().replace(/\.[A-Z]*/g,"").includes(string.toUpperCase()))) {
+				el.style.display="";
+				el.parentNode.style.display="";
 				if (input.value == "") {hide()};
 			} else {
-                tab[X].style.display="none";
+                el.style.display="none";
 			}
 		}
 	};
 
-	function hide(state){
-		if (!(state)) {var state=""}
-		var tab = document.getElementById("layoutsTable").getElementsByClassName("ms-rte-layoutszone-inner")[0].childNodes;
-		for (var X=0; X<tab.length; X++) {
-			if (tab[X].data && (tab[X].data.includes("\n"))) {continue};
-			tab[X].style.display=state;
+	function hide(arg){
+        var state=''
+		if (arg) {state=arg;}
+		var els = document.getElementById("layoutsTable").getElementsByClassName("ms-rte-layoutszone-inner")[0].childNodes;
+		 for (var el of els) {
+			if (el.data && (el.data.includes("\n"))) {continue};
+			el.style.display=state;
 		}
 	};
 
-	if (window.location.href.includes('sites/CommandCenter2/wiki/SitePages/Home.aspx')) {
-		var x = document.getElementById("layoutsTable").insertRow(0);
-		x.id="myRowForSearch";
-		var y = x.insertCell(0);
-		y.id="myCellForSearch";
-		y.style.textAlign="center";
-		var input = document.createElement('input');
-		input.id="myInputForSearch";
-		input.placeholder="Cerca";
-		input.oninput = () => {search(input.value)};
-		y.appendChild(input);
-		input.focus();
-		var css = window.document.styleSheets[0];
-		css.insertRule('#myInputForSearch[placeholder] {text-align: center;}', css.cssRules.length);
-	}
     document.title = document.title.replace(/commandcenter - /gi,"");
+	if (window.location.href.includes('sites/CommandCenter2/wiki/SitePages/Home.aspx')) {
+        document.addEventListener("keypress", function (e) {if (e.key==='Enter'){null}})
+        var width = document.getElementById("layoutsTable").clientWidth
+        var div = document.createElement('div');
+        div.id = 'myDivForSearch'
+        div.classList.add("cssInputContainer");
+        var input = document.createElement('input');
+		input.id="myInputForSearch";
+        input.placeholder="Cerca";
+		input.oninput = () => {search(input.value)};
+        $(div).appendTo("body");
+		$(input).appendTo(div);
+        $(input).focusout(function(){input.focus()});
+        $(".cssInputContainer").css({"display": "block", "position": "fixed", "bottom": "10px", "left": "3px", "font-size": "10px", "padding": "20px","user-select":"none"});
+        $("#myInputForSearch").css({"text-align": "", "width": "150px", "font-size": "20px"});
+        input.focus();
+	}
 
 })();
