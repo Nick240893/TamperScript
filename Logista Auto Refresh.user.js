72aa55fd1e5530e221823fcb4cd311f285e7c993
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-09-01 03:50:08 +0200

    Update Logista Auto Refresh.user.js

--- Logista Auto Refresh.user.js
+++ Logista Auto Refresh.user.js
@@ -1,24 +1,20 @@
 // ==UserScript==
 // @name         Logista Auto Refresh
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       NigroN
-// @match        https://www.logista.com/*
+// @match        https://www.logista.com/sm/index.do*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=logista.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
+    var timer=120; //secondi
 	function refresh() {
-        var el = document.getElementsByClassName("x-btn-text");
-		for (var X=0; X<el.length; X++) {
-			if (el[X].innerText.includes("Refresh")) {
-				el[X].click();
-                setTimeout(refresh, 30000);
-			}
-		}
+        var els = document.getElementsByClassName("x-btn-text");
+		for (var el of els) {if (el.innerText.includes("Refresh")) {el.click()}};
+         console.log("Ultima Esecuzione: " + ('0' + new Date().getHours()).slice(-2) +":"+ ('0' + new Date().getMinutes()).slice(-2) +":"+ ('0' + new Date().getSeconds()).slice(-2))
 	}
-
-	refresh();
+	setInterval(refresh, Math.round(timer*1000));
 })();
