23962b203a8ff7f4af18a46e6016725c7fc286d8
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-09-12 04:07:05 +0200

    Update ControlM Auto Refresh.user.js

--- ControlM Auto Refresh.user.js
+++ ControlM Auto Refresh.user.js
@@ -1,19 +1,20 @@
 // ==UserScript==
 // @name         ControlM Auto Refresh
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      1.0
 // @description  try to take over the world!
 // @author       NigroN
 // @match        http://cattolica-icinga1/ControlM/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @require      https://checkapp2.pages.dev/Dipendenze/jQueryUI-Cupertino/external/jquery/jquery.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     var timer=120; //secondi
     function reload() {
-        document.getElementsByClassName("d-button d-button_action d-icon-refresh ng-scope")[1].click();
-        setTimeout(function(){console.log("Ultima Esecuzione: " + ('0' + new Date().getHours()).slice(-2) +":"+ ('0' + new Date().getMinutes()).slice(-2) +":"+ ('0' + new Date().getSeconds()).slice(-2))}, 1000);
+        $('button.d-button.d-button_action.d-icon-refresh.ng-scope').click()
+        console.log("Ultima Esecuzione: " + ('0' + new Date().getHours()).slice(-2) +":"+ ('0' + new Date().getMinutes()).slice(-2) +":"+ ('0' + new Date().getSeconds()).slice(-2));
     }
 setInterval(reload, Math.round(timer*1000));
 })();
