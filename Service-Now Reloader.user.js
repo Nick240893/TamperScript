755922b7fe72e08780b343550c2455c1e7e76684
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-09-18 15:13:15 +0200

    Update Service-Now Reloader.user.js

--- Service-Now Reloader.user.js
+++ Service-Now Reloader.user.js
@@ -1,23 +1,23 @@
 // ==UserScript==
 // @name         Service-Now Reloader
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.5
 // @description  try to take over the world!
 // @author       NigroN
-// @include      https://*.service-now.com/*
-// @include      http://*.service-now.com/*
+// @include      https://*.service-now.com/*home.do*
+// @include      https://*.service-now.com/*dashboard.do*
+// @include      https://*.service-now.com/*navpage.do*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    var timer=120; //secondi
     function loop() {
-        if (document.visibilityState != "visible") {
-            if ((/navpage\.do|home\.do|dashboard\.do/.test(window.location.href))) {
-                document.location.reload(true);
-            }
+        if (document.visibilityState != "visible" && (/navpage\.do|home\.do|dashboard\.do/.test(window.location.href))) {
+            try {homeRefresh()} catch {document.location.reload(true)}
         }
     }
+
+    var timer=120; //secondi
     setInterval(loop, Math.round(timer*1000));
     setTimeout(function(){console.log("Ultima Esecuzione: " + ('0' + new Date().getHours()).slice(-2) +":"+ ('0' + new Date().getMinutes()).slice(-2) +":"+ ('0' + new Date().getSeconds()).slice(-2))}, 1000)
 })();
