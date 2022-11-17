978c69588689ea607f31939e7eaedef0dfabfe4a
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-11-17 09:47:07 +0100

    Update FixSplunk.user.js

--- FixSplunk.user.js
+++ FixSplunk.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         FixSplunk
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       NigroN
 // @include      https://mooneycloud.splunkcloud.com/it-IT/app/cr_event_management/console_events*
-// @include      https://mooneycloud.splunkcloud.com/en-US/app/cr_event_management/console_events*
 // @require      https://checkapp2.pages.dev/Dipendenze/jQueryUI-Cupertino/external/jquery/jquery.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
-setInterval(fix, 1000);
+setInterval(fix, 500);
 
 function fix() {
-    $('.modal.fade.external-url-modal.shared-dialogs-externalurlmodal.in').remove();
-    $('.modal-backdrop.fade.in').remove();
+$('.btn.btn-primary.modal-btn-confirm').each(function(i){
+    if (this.parentNode.parentNode.innerText.includes('Continue with external content')) {this.click()}
+})
 }
 })();
