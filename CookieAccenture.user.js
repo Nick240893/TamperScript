772a33f118c714d5a9bc0ec3cd0d9ed9213a9d6e
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2022-12-27 13:14:38 +0100

    update CookieAccenture.user.js

--- CookieAccenture.user.js
+++ CookieAccenture.user.js
@@ -9,10 +9,9 @@ function cookieList() {
 GM_cookie('list', { name: "io_chs_italy_sso" }, (cookies) => {
     if (cookies[0].sameSite != "no_restriction") {
         GM_cookie('delete',cookies[0])
-        
+        cookies[0].sameSite = "no_restriction"
+        GM_cookie('set',cookies[0])
     }
-	console.log("testo1");
-	console.log(JSON.stringify(cookies));
     unsafeWindow.cookies=cookies;
 });
 }
