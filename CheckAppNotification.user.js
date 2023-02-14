fa7385015b421cd86d04e75360d46d75265bbed2
Nick240893 <99483483+Nick240893@users.noreply.github.com>
2023-02-14 15:01:29 +0100

    Update CheckAppNotification.user.js

--- CheckAppNotification.user.js
+++ CheckAppNotification.user.js
@@ -4,11 +4,10 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       NigroN
-// @includes     file:///*
-// @includes     https://checkapp2.pages.dev/*
-// @grant        GM_notification
+// @match        https://checkapp2.pages.dev/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
 // ==/UserScript==
-
 (function() {
     function jsSysNotification(a,b) {GM_notification({title:a,text:b})}
     unsafeWindow.jsSysNotification=jsSysNotification;
